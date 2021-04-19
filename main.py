7460583d1cd968a7a71404a53be6347b620dddd5
ashita8 <65624440+ashita8@users.noreply.github.com>
2021-04-19 21:52:46 +0530

    Quiz game
    only using python.

--- main.py
+++ main.py
@@ -1,139 +1,91 @@
-from tkinter import *
+# quiz app
+import random
+import matplotlib.pylab
+questions = {1: "What is the Full Form of INTERNET ?", 2: "which one is a computer language",
+             3: "Ctrl, Shift and Alt are called .......... keys.",
+             4: "MS-Word is an example of _____", 5: "A computer cannot boot if it does not have the ___",
+             6: "_______ is the process of dividing the disk into tracks and sectors",
+             7: "Junk e-mail is also called ______",
+             8: "____are attempts by individuals to obtain confidential information from you by falsifying their identity",
+             9: "Microsoft Office is an example of a", 10: "By default, your documents print in ________ mode", }
 
-class Application(Frame):
+answers = {1: 'a', 2: 'a', 3: 'a', 4: 'c', 5: 'c', 6: 'b', 7: 'a', 8: 'a', 9: 'c', 10: 'b'}
+index = []
+options = {1: ['A)INTERnational-NETwork which is the interconnected network of all the Web Servers worldwide ',
+               'B)International network of science',
+               'C) INTERNATIONAL NET WORTH OF INDIA',
+               'D) INTERNET WORKS HERE'],
+           2: ['A) c++', 'B) mango', 'C) tupple', 'D) rw'],
+           3: ['A) modifier	', 'B) function', 'C) alphanumeric', 'D) adjustment'],
+           4: ['A) An operating system', '	B) A processing device',
+               'C) Application software', '	D) An input device'],
+           5: ['A) Compiler', '	B) Loader',
+               'C) Operating system', '	D) Assembler'],
+           6: ['A) Tracking', '	B) Formatting',
+               'C) Crashing', '	D) Allotting'],
+           7: ['A) Spam	', 'B) Spoof',
+               'C) Sniffer script', '	D) Spool'],
+           8: ['A) Phishing trips', '	B) Computer viruses',
+               'C) Phishing scams', '	D) Spyware scams'],
+           9: ['A) Closed source software', '	B) Open source software',
+               'C) Horizontal market software', '	D) vertical market software'],
+           10: ['A) Landscape	', 'B) Portrait',
+                'C) Page Setup', '	D) Print View'],
+           }
 
-    def __init__(self,master):
-
-        super (Application,self).__init__(master)
-        self.task = ""  #future class subclass which have tasks in future
-        self.UserIn = StringVar()
-        self.grid()
-        self.create_widgets() #.grid() is used to register widgets the grid geometry manager
-                            # geometry manager manages the placement and layout of the elements of GUI
-
-    def create_widgets(self):
-        self.user_Input = Entry(self, bg="#5BC8AC", bd=29,
-                                insertwidth=4, width=24,
-                                font=("Verdana", 20, "bold"), textvariable = self.UserIn, justify= RIGHT)
-        self.user_Input.grid(columnspan=4)
-
-        self.user_Input.insert(0, "0")
-        self.button1 = Button(self, bg="#98DBC6", bd=12,
-                              text="1", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(1))
-        self.button1.grid(row=2, column=0, sticky=W)
-
-        self.button2 = Button(self, bg="#98DBC6", bd=12,
-                              text="2", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(2))
-        self.button2.grid(row=2, column=1, sticky=W)
-
-        self.button3 = Button(self, bg="#98DBC6", bd=12,
-                              text="3", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(3))
-        self.button3.grid(row=2, column=2, sticky=W)
-
-        self.button4 = Button(self, bg="#98DBC6", bd=12,
-                              text="4", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(4))
-        self.button4.grid(row=3, column=0, sticky=W)
-
-        self.button5 = Button(self, bg="#98DBC6", bd=12,
-                              text="5", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(5))
-        self.button5.grid(row=3, column=1, sticky=W)
-
-        self.button6 = Button(self, bg="#98DBC6", bd=12,
-                              text="6", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(6))
-        self.button6.grid(row=3, column=2, sticky=W)
-
-        self.button7 = Button(self, bg="#98DBC6", bd=12,
-                              text="7", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(7))
-        self.button7.grid(row=4, column=0, sticky=W)
-
-        self.button8 = Button(self, bg="#98DBC6", bd=12,
-                              text="8", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(8))
-        self.button8.grid(row=4, column=1, sticky=W)
-
-        self.button9 = Button(self, bg="#98DBC6", bd=12,
-                              text="9", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(9))
-        self.button9.grid(row=4, column=2, sticky=W)
-
-        self.button0 = Button(self, bg="#98DBC6", bd=12,
-                              text="0", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                              command=lambda: self.buttonClick(0))
-        self.button0.grid(row=5, column=0, sticky=W)
-
-        self.addition = Button(self, bg="#98DBC6", bd=12,
-                               text="+", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                               command=lambda: self.buttonClick("+"))
-        self.addition.grid(row=2, column=3, sticky=W)
-
-        self.Subbutton = Button(self, bg="#98DBC6", bd=12,
-                                text="-", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                                command=lambda: self.buttonClick("-"))
-        self.Subbutton.grid(row=3, column=3, sticky=W)
-
-        self.Multbutton = Button(self, bg="#98DBC6", bd=12,
-                                 text="*", padx=33, pady=25, font=("Helvetica", 20, "bold"),
-                                 command=lambda: self.buttonClick("*"))
-        self.Multbutton.grid(row=4, column=3, sticky=W)
-
-        self.Divbutton = Button(self, bg="#98DBC6", bd=12,
-                                text="/", padx=33, pady=25,
-                                command= lambda: self.buttonClick("/"), font=("Helvetica", 20, "bold"))
-        self.Divbutton.grid(row=5, column=3, sticky=W)
+def showresult(score):
+    if score >=30:
+        print('********** PASS **********')
+        print('**** Your score is: '+str(score)+' out of 50 ****\n')
+        #print('**** Number of correct answers ='+ str(count)+' ****\n\n')
 
 
-        self.equalbutton = Button(self, bg="#E6D72A", bd=12,
-                                  text="=", padx=100, pady=25,
-                                  command = self.CalculateTask, font=("Helvetica", 20, "bold"))
-
-        self.equalbutton.grid(row=5, column=1, sticky=W,columnspan = 2)
-
-        self.clearbutton = Button(self, bg="#E6D72A", bd=12,
-                                  text="AC",font=("Helvetica", 20, "bold"),width = 28,padx = 7,command = self.ClearDisplay )
-
-        self.clearbutton.grid(row=1, columnspan=4, sticky=W)
-
-    def buttonClick(self, number):
-        self.task = str(self.task) + str(number)
-        self.UserIn.set(self.task)
-
-    def CalculateTask(self):
-        self.data = self.user_Input.get()
-
-        try:
-            self.answer = eval(self.data)
-            self.displayText(self.answer)
-            self.task = self.answer
-
-        except SyntaxError as e:
-            self.displayText("Invald syn")
-            self.task = ""
-
-    def displayText(self, value):
-        self.user_Input.delete(0, END)
-        self.user_Input.insert(0, value)
-
-    def ClearDisplay(self):
-        self.task = ""
-        self.user_Input.delete(0, END)
-        self.user_Input.insert(0, "0")
+    else:
+        print('better luck next time')
+        print('**** Your score is: '+str(score)+' out of 50 ****\n\n')
 
 
-calculator = Tk()
-calculator.title("Calculator")
-app= Application(calculator)
-calculator.resizable(width= False,height= False)
+def startquiz(arr):
+    global count
+    print(arr)
+    score = 0
+    for i in range(0, 5, 1):
+        p = arr[i]
+        print('Question ' + str(i + 1) + ': ' + questions[p]+'\n')
+        print('**** type a,b,c,d of selected options: ****\n')
+        print(options[p])
+        ans = input('your answer: ')
 
-calculator.mainloop()
+        if ans == answers[p]:
+
+            score = score + 10
+        else:
+            score = score-5
+        print("\n \n")
+
+    showresult(score)
+check = 1
+while check == True:
+    choice = int(input('enter choice 1 ,2 or 3\n 1 for start game \n 2 for rules\n 3 for exit \n'))
+    if choice == 1:
+        print('****** start game ******\n')
+        for i in range(10):
+            r = random.randint(1, 10)
+            if r not in index and len(index) < 5:
+                index.append(r)
+
+            elif len(index) == 5:
+                break
+            else:
+                continue
+        startquiz(index)
 
 
-
-
-
+    elif choice==2:
+        print(
+            'rules: \n 1. you will get 5 questions \n2. if you answer correctly you will get 10 marks if you answered wrong 5 marks will be deducted\n'
+            '3. if you score more than 30 you will pass\n ')
+    elif choice ==3:
+        break
+    else:
+        exit()
